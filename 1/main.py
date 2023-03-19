6710198fa14dfdd0fc612be55bf839ee2e06b709
dep1220 <115685401+dep1220@users.noreply.github.com>
2023-03-19 15:48:06 +0700

    Delete 1 directory

--- 1/main.py
+++
@@ -1,9 +0,0 @@
-# pengenalan variabel di python..
-a = 10 
-b = 200
-c = 3000
-
-print("nilai dari", a, " + ", b, "+", c, " = ",a + b + c)
-print("nilai dari", a, " + ", b, "+", c, " = ",a + b + c) # alt + shift + panah
-
-print(a,b,a)
